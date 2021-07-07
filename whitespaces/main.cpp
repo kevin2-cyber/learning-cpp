3a801b31fc4a3c4c21a891d569499db291cfc1e5
kevin2-cyber <kimikevin@zoho.com>
2021-07-07 17:43:19 +0100

    assignment

--- whitespaces/main.cpp
+++ whitespaces/main.cpp
@@ -4,12 +4,15 @@ using namespace std;
 
 int main()
 {
-    int money;
+    int numPeople;
 
-    cout << "How much do you owe? ";
-    cin >> money;
-    cout << "You owe " << money << " cedis" << endl;
-    cout << 4- -2;
+    cout << "Enter the number of the people: ";
+
+    cin >> numPeople;
+
+    numPeople = numPeople + 5;
+
+    cout << "There are " << numPeople << " people." << endl;
 
     return 0;
 }
